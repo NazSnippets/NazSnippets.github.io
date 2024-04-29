fd55612a6d67ebed7219314f97a39214068e81ec
Nazmul <nazmulkabIr2006@gmail.com>
2024-04-30 00:01:31 +0600

    top30

--- fakesubslb/assets/script.js
+++ fakesubslb/assets/script.js
@@ -5,7 +5,7 @@ var lol = []
 for (var l = 1; l <= 3; l++) {
   var htmlrow = `<div class="row_${l} row"></div>`;
   $('.counters').append(htmlrow);
-    for (var t = 1; t <= 9; t++) {
+    for (var t = 1; t <= 10; t++) {
       let number;
       if(c.toString().length == 1) {
           number = `<div class="cnb">0${c}</div>`
@@ -15,7 +15,7 @@ for (var l = 1; l <= 3; l++) {
 
       var htmlcard = `<div class="channel_${c} card" id="card_thing_${c}">
       ${number}
-      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" class="cimg">
+      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" class="cimg" id="img_${c}">
       <div class="chnam" id="name_${c}">Loading</div>
       <div class="subscriberCount odometer" id="count_${c}">0</div>
       </div>`;
@@ -25,23 +25,18 @@ for (var l = 1; l <= 3; l++) {
 }
 
 function updateData(data) {
-  for (var l = 1; l <= 28; l++) {
+  for (var l = 1; l <= 30; l++) {
   
-  document.getElementById(`name_${l}`).innerText = data[l].name;
-  document.getElementById(`count_${l}`).innerText = data[l].clicks;
+  document.getElementById(`name_${l}`).innerText = data[l-1].name;
+  document.getElementById(`count_${l}`).innerText = data[l-1].clicks;
 }
 }
-
-
 function update(){
   $.getJSON('https://c20356d0-058e-4686-b3c7-8bb6a84b5c2e-00-1xmiixkedfevd.sisko.replit.dev/api',(data)=>{
 
       data.sort(function(a,b){return b.clicks - a.clicks});
-
-      
         updateData(data)
   });
-
 }
 
 update()
